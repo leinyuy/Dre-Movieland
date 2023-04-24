b502d34b5358229082777bc0efe702d8d46a6f0c
Dre Part <57998158+leinyuy@users.noreply.github.com>
2023-04-24 15:01:37 +0100

    Update App.js

--- dre-movieland/src/App.js
+++ dre-movieland/src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
+import "./App.css";
+
+const Person = () => {
+  return (
+    <>
+      <h1> Name: John</h1>
+      <h2>Last Name: Doe</h2>
+      <h2>Age: 30</h2>
+    </>
+  );
+};
 
 const App = () => {
-  const name = 'john';
-  const isName = true;
-  return (
-    <div className="App">
-      <h1>Hello { isName? name: 'Someone'}!</h1>
+  return(
+  <div className="App">
+    <Person />
     </div>
-  );
-}
+    )
+};
+
+
+
 
 export default App;
