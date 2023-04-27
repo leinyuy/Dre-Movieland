4b98b655fa4d1d1ccf541663694ee5c864964a1a
Dre Part <57998158+leinyuy@users.noreply.github.com>
2023-04-27 20:19:21 +0100

    succesfully fetched

--- dre-movieland/src/App.js
+++ dre-movieland/src/App.js
@@ -1,26 +1,26 @@
-import "./App.css";
+import React, { useEffect } from 'react';
 
-const Person = (props) => {
-  return (
-    <>
-      <h1> Name: {props.name}</h1>
-      <h2>Last Name: {props.lastName}</h2>
-      <h2>Age: {props.age}</h2>
-    </>
-  );
-};
+
+
+//f2e4937a
+const API_URL = 'http://www.omdbapi.com?apikey=f2e4937a';
+
 
 const App = () => {
-  return(
-  <div className="App">
-      <Person name={'John'} lastName={'remy'} age={ '25'} />
-      <Person />
-      <Person />
-    </div>
-    )
-};
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data)
+    }
 
+    useEffect(() => {
+        searchMovies('Spiderman');
 
+    },[])
 
+    return (
+        <h1>App</h1>
+    );
+}
 
 export default App;
