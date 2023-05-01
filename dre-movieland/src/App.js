536ddf4d28c995c2c3e9f5e5a0c6321dad59614c
Dre Part <57998158+leinyuy@users.noreply.github.com>
2023-05-01 13:18:42 +0100

    finally added the multiply results feature

--- dre-movieland/src/App.js
+++ dre-movieland/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
@@ -6,29 +6,26 @@ import MovieCard from './MovieCard';
 //f2e4937a
 const API_URL = 'http://www.omdbapi.com?apikey=f2e4937a';
 
-const movie1 = {
-    "Title": "Superman, Spiderman or Batman",
-    "Year": "2011",
-    "imdbID": "tt2084949",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
-}
 
 const App = () => {
+    const [movies, setMovies] = useState([]);
+
+
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
-        console.log(data.search)
+        setMovies(data.Search)
+        console.log(data)
     }
 
     useEffect(() => {
-        searchMovies('Spiderman');
+        searchMovies('batman');
 
     }, []);
 
     return (
         <div className="app">
-            <h1>DreLand</h1>
+            <h1>DreMovie Search</h1>
             <div className="search">
                 <input
                     pladceholder="search for movies"
@@ -40,9 +37,19 @@ const App = () => {
                     onClick={()=>{}}
                 />
             </div>
-            <div className="container">
-                <MovieCard movie1={movie1} />
-            </div>
+            {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+           
+            
 
         </div>
     );
