0fe7fb1cd50e909bc1f83e97fbd6ebbe9f817335
Dre Part <57998158+leinyuy@users.noreply.github.com>
2023-05-01 14:30:42 +0100

    done with the simple search site

--- dre-movieland/src/App.js
+++ dre-movieland/src/App.js
@@ -9,7 +9,7 @@ const API_URL = 'http://www.omdbapi.com?apikey=f2e4937a';
 
 const App = () => {
     const [movies, setMovies] = useState([]);
-
+    const[searchTerm, setSearchTerm]=useState("")
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
@@ -24,34 +24,31 @@ const App = () => {
     }, []);
 
     return (
-        <div className="app">
-            <h1>DreMovie Search</h1>
-            <div className="search">
-                <input
-                    pladceholder="search for movies"
-                    value="superman"
-                    onChange={ ()=>{}}
-                />
+      <div className="app">
+        <h1>DreMovie Search</h1>
+        <div className="search">
+          <input
+            pladceholder="search for movies"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
                 <img src={SearchIcon}
                     alt="search"
-                    onClick={()=>{}}
-                />
-            </div>
-            {movies?.length > 0 ? (
-        <div className="container">
-          {movies.map((movie) => (
-            <MovieCard movie={movie} />
-          ))}
+                    onClick={() => searchMovies(searchTerm)} />
+        
         </div>
-      ) : (
-        <div className="empty">
-          <h2>No movies found</h2>
-        </div>
-      )}
-           
-            
-
-        </div>
+        {movies?.length > 0 ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )}
+      </div>
     );
     
 }
