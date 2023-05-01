64ff5ca6f36fd9ffa52cdd0ca6a430e42391b946
Dre Part <57998158+leinyuy@users.noreply.github.com>
2023-05-01 11:25:55 +0100

    just created a new movie card jsx file to hold the movie card component

--- dre-movieland/src/App.js
+++ dre-movieland/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import SearchIcon from './search.svg';
-
+import MovieCard from './MovieCard';
 
 //f2e4937a
 const API_URL = 'http://www.omdbapi.com?apikey=f2e4937a';
@@ -18,7 +18,7 @@ const App = () => {
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
-        console.log(data)
+        console.log(data.search)
     }
 
     useEffect(() => {
@@ -41,18 +41,7 @@ const App = () => {
                 />
             </div>
             <div className="container">
-                <div className="movie">
-                    <div>
-                        <p>{movie1.Year}</p>
-                    </div>
-                    <div>
-                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt ={movie1.Title}/>
-                    </div>
-                    <div>
-                        <span>{movie1.Type}</span>
-                        <h3>{movie1.Title}</h3>
-                    </div>
-            </div>
+                <MovieCard movie1={movie1} />
             </div>
 
         </div>
